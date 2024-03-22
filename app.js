7bc9686ba0b446d1d6cbde302d72eb0712566489
MichalPresz <m.presz@wp.pl>
2024-03-22 15:07:54 +0000

    edits for Friday

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ alert("Welcome to our company website,  " + visitor +"!");
 let Home = prompt("Do you want to learn how to make Atoms?");
 if (Home == "yes") {
   alert("Okay, let`s get to work!");
-} else {
+
+} else 
+if (Home == "no"){
   alert("Okay, maybe next time!");
 }
