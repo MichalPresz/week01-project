1ef111ae64fd38abfab6c12f02c481ec6c016c2c
MichalPresz <m.presz@wp.pl>
2024-03-22 14:46:56 +0000

    change of text

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Hello")
 let visitor = prompt("Please enter your name:");
-alert("Welcome to my website,  " + visitor +"!");
+alert("Welcome to our company website,  " + visitor +"!");
 let Home = prompt("Do you want to learn how to make Atoms?");
 if (Home == "yes") {
   alert("Okay, let`s get to work!");
