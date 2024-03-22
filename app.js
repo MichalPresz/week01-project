da9b428d7fc63958538cf83eb0aea7d0e5a8699c
MichalPresz <m.presz@wp.pl>
2024-03-22 14:45:02 +0000

    jss prompt

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hello")
+let visitor = prompt("Please enter your name:");
+alert("Welcome to my website,  " + visitor +"!");
+let Home = prompt("Do you want to learn how to make Atoms?");
+if (Home == "yes") {
+  alert("Okay, let`s get to work!");
+} else {
+  alert("Okay, maybe next time!");
+}
