bd1c00431a97eed5a04d1175d8535baa33471740
MichalPresz <m.presz@wp.pl>
2024-03-24 07:52:38 +0000

    .js change1

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log("Hello")
+console.log("Hello");
 let visitor = prompt("Please enter your name:");
 alert("Welcome to our company website,  " + visitor +"!");
 let Home = prompt("Do you want to learn how to make Atoms?");
-if (Home == "yes") {
+if (Home == "yes","Yes") {
   alert("Okay, let`s get to work!");
-
 } else 
-if (Home == "no"){
+if (Home == "no","No"){
   alert("Okay, maybe next time!");
 }
